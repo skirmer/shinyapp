64c16943dd3ffea6dd1cc4e7222312276d0f7f9e
Stephanie Kirmer <skirmer@Stephanies-MacBook-Pro.local>
2017-04-08 09:29:19 -0500

    App showing aldermanic voting records

---
+++ chivotes/app.R
@@ -0,0 +1,108 @@
+# Data 4 Democracy Chicago Lobbying Project #
+# App: Votes by Aldermen #
+# S. Kirmer #
+# April 2017 #
+
+# Set up location
+# setwd("U://Github/shinyapp/firstapp/")
+# Put in the token data from shinyapps.io profile
+# setAccountInfo()
+
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(DT)
+library(shinythemes)
+library(rsconnect)
+library(ggvis)
+library(plotly)
+
+df <- read.csv("https://query.data.world/s/bqziq63nd9vdt5e7ph3htop6c",header=T, stringsAsFactors=FALSE)
+
+# 
+# for(i in 1:nrow(df)){
+#   for(n in 1:length(oldald)){
+#     ifelse(grepl(oldald[n], df[i, "Alderman"]),
+#            df[i, "alderman2"] <- as.character(oldald[n]), NA)
+#   }
+# }
+
+#Bit of cleanup#
+df$Vote <- ifelse(df$Vote == "AO’Connor", "A", df$Vote)
+df$Vote <- ifelse(df$Vote %in% c("ErvinY","PawarY","SmithY"), "Y", df$Vote)
+df <-dplyr::filter(df, Vote %in% c("Y", "N", "A", "E", "NV", "V"))
+
+
+
+
+df2 <- unique(df[,c("Alderman","Ward", "Vote", "Date","Record", "Title")])
+df2$Ward <- as.numeric(df2$Ward)
+
+server <- function(input, output) {
+  #the server - literally what data is going into the plot/viz?
+  
+  output$table <- DT::renderDataTable(DT::datatable(rownames = FALSE,
+                                                    {
+                                                      data <- df2
+                                                      
+                                                      if(input$Alderman != "All"){
+                                                        data <- dplyr::filter(data, Alderman == input$Alderman)
+                                                      }
+                                                      
+                                                      if(input$Ward != "All") {
+                                                        data <- dplyr::filter(data, Ward == input$Ward) 
+                                                      }
+                                                      
+                                                      if(input$Record != "All") {
+                                                        data <- dplyr::filter(data, Record == input$Record) 
+                                                      }
+                                                      
+                                                      if(input$Vote != "All") {
+                                                        data <- dplyr::filter(data, Vote == input$Vote) 
+                                                      }
+                                                      
+                                                      data}))
+}
+
+
+
+
+ui <- fluidPage(
+  theme = shinytheme('lumen'),
+  titlePanel("Aldermanic Voting Records"),
+  
+  "This page is part of the Data4Democracy Chicago Lobbying project. This data comes from the City of Chicago Data Portal.",
+  br(),br(),
+  "Key to votes: Y=yes | N=no | A=absent | NV=not voting | V=Vacant | E= Excused",
+  br(),br(),
+  "Join us at ", a("data.world/lilianhj/chicago-lobbyists!", href="https://data.world/lilianhj/chicago-lobbyists"),
+  br(),br(),
+  
+  fluidRow(
+    column(3,
+           selectInput("Alderman",
+                       "Alderman:",
+                       c("All",  sort(trimws(unique(as.character(df$Alderman))))))),
+    column(3,
+           selectInput("Ward",
+                       "Ward:",
+                       c("All", sort(trimws(unique(as.character(df$Ward))))))),
+    column(2,
+           selectInput("Record",
+                       "Record:",
+                       c("All", sort(trimws(unique(as.character(df$Record))))))),
+    
+    column(3,
+           selectInput("Vote",
+                       "Vote:",
+                       c("All", sort(trimws(unique(as.character(df$Vote)))))))
+  )
+  ,
+  fluidRow(
+    DT::dataTableOutput("table")
+  )
+
+)
+
+shinyApp(ui = ui, server = server)
