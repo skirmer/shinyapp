658deb980247e3f8b8e6d3bb59b2a50215f75a24
Stephanie Kirmer <skirmer@Stephanies-MacBook-Pro.local>
2018-04-10 22:00:44 -0500

    Updating site

---
+++ stephanie/archive/app_old.R
@@ -0,0 +1,48 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(DT)
+library(shinythemes)
+library(rsconnect)
+library(ggvis)
+library(plotly)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  theme = shinytheme('lumen'),
+   # Application title
+   titlePanel("Stephanie Kirmer"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ stephanie/resume_backlog.R
@@ -0,0 +1,81 @@
+#### Data Scientist, Uptake
+#### May 2017 - present  
+
+I work in the Rail industry vertical at Uptake, building predictive models to diagnose failures in diesel locomotives. I also build R packages and other software tools to improve our team's efforts in data cleaning, exploratory analysis, and modeling. I regularly work directly with clients, and give internal presentations from time to time about my work and the tools I build. 
+
+Key accomplishments:
+
+* Build and maintain high performing (AUC > .75) predictive models for direct value add to clients  
+* Helped pilot an internal training program for teams to learn about the work of data science, helping areas such as sales better understand the business  
+* Built an R package to manage subject matter expert feedback requests and facilitate improving data labels for modeling  
+* Manage subject matter expert interactions with data scientists for the rail line of business day to day, facilitating strong relationships with customer  
+
+<BR>
+
+***
+
+<BR>
+
+#### Research Analyst, University of Chicago Urban Labs  
+#### October 2015 - April 2017  
+
+Produce complex original analyses using R, SAS, Python, Stata, and other statistical programming interfaces as part of academic research on the efficacy of social programs and interventions. Content area specialist in education, with additional experience in crime, poverty, and health data. Technical skills in regression modeling, causal estimation, and RCT analysis.
+
+Key accomplishments:
+
+* Conduct statistical analysis using multivariate linear modeling, logistic regression modeling, 2-stage least squares models, and more in support of a forthcoming report evaluating the efficacy of a tutoring program.  
+* Supervision of junior staff and interns on analytic projects, including technical training in R and SAS.  
+* Production of complex graphs and GIS visualizations in support of various projects.  
+* Feature engineering and model construction/testing using gradient boosting, tree/forest techniques, and linear and logistic models.  
+* Establishment of team best practices surrounding reproducible research using R and LaTeX, and training staff on using these interfaces.  
+
+<BR>
+
+***
+
+<BR>
+
+#### Data Analyst, University of Chicago Consortium on Chicago School Research (CCSR)  
+#### March 2014 – October 2015   
+
+Liaison between CCSR, UChicago Urban Labs, and Chapin Hall researchers and Chicago Public Schools on data analytics. Provided support and content area expertise on technical data manipulation and querying, legal and student privacy matters, and research study design.
+
+Key accomplishments:
+
+* Filled over three dozen individual data requests annually for researchers, including writing complex SAS/SQL queries from relational database and cleaning/manipulating data  
+* Collaborated with researchers on writing of statements of work/data sharing agreements for 8 research studies  
+* Built web-based system for submitting data requests and constructed workflow to queue and fill requests  
+* Created and maintained first ever data dictionary on CPS data, including comprehensive wiki repository of knowledge about CPS data policies and procedures  
+* Gave 10 presentations annually to research teams on key CPS data issues and procedures   
+
+<BR>
+
+***
+
+<BR>
+
+#### Project Manager, International Programs, DePaul University  
+#### 2010 – 2014   
+
+Research Assistant, International Programs, DePaul University   
+2010   
+
+Conducted research and data analysis on international students and study abroad participant students for DePaul University, producing reports for university leadership. Managed direct reports. Promoted during period of employment.
+
+Key accomplishments: 
+
+* Built first ever Tableau visualization workbooks detailing international student trends and academic outcomes  
+* Coordinated annual international student satisfaction survey with outside vendor, and conducted independent analysis of survey results  
+* Managed study abroad program management database and application system (StudioAbroad platform), including training new staff and faculty on using system  
+
+<BR>
+
+***
+
+<font size = "2">
+Analyst, Thomson Reuters  
+2008 - 2009   
+
+Conducted data analysis with internal, proprietary data analysis system about employee health benefits for corporate clients. 
+
+</font>
